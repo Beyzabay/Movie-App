52fd26b916b03b6bab43ed2d771f5612612e9199
Beyza Bay <beyza.bay@icloud.com>
2024-06-15 22:41:32 +0300

    update page

--- movie-app/src/app/page.jsx
+++ movie-app/src/app/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Movies from '../components/Movies';
+import Movies from './components/Movies';
 
 const page = async({searchParams}) => {
   const res = await fetch(`https://api.themoviedb.org/3/${searchParams.genre ? "movie/" + searchParams.genre : "trending/all/day"}?api_key=bf0c5a689718187dcbff0b2917cc399e&language=en-US&page=1`, {next: {revalidate:1000}})
